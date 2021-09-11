4775da8da9f8bf677d9bcb2498b171cb5e308d5b
Adalid Alanoca <adalidalanoca2029@gmail.com>
2021-09-10 20:01:20 -0400

    Commit

--- app/Controllers/Personal.php
+++ app/Controllers/Personal.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Libraries\SSP;
+
 
 class Personal extends BaseController
 {
@@ -13,5 +15,24 @@ class Personal extends BaseController
 	public function index()
 	{
 		return $this->templater->view('personal/listarPersonal');
+	}
+	public function ajaxListarAsignacionesMaestros()
+	{
+		// print_r($_REQUEST);
+		$table = <<<EOT
+        (SELECT * from pb_persona) temp
+        EOT;
+		$primaryKey = 'id_materia_maestro';
+		$where = "estado = 1 and gestion = " . date('Y');
+		$columns = array(
+			array('db' => 'id_materia_maestro', 'dt' => 0),
+			array('db' => 'nivel', 'dt'            => 1),
+			array('db' => 'materia', 'dt'            => 2),
+			array('db' => 'nombre_completo', 'dt'    => 3),
+			array('db' => 'gestion', 'dt'            => 4),
+		);
+
+		$sql_details = array('user' => $this->db->username, 'pass' => $this->db->password, 'db'   => $this->db->database, 'host' => $this->db->hostname);
+		return $this->response->setJSON(json_encode(SSP::complex($_GET, $sql_details, $table, $primaryKey, $columns, $where)));
 	}
 }
--- app/Views/base.php
+++ app/Views/base.php
@@ -103,12 +103,12 @@
 	<!--end::Main-->
 	<!--begin::Javascript-->
 	<!--begin::Global Javascript Bundle(used by all pages)-->
+	<script src="<?= base_url('metronic/assets/js/jquery-3.6.0.min.js') ?>"></script>
 	<script src="<?= base_url('metronic/assets/plugins/global/plugins.bundle.js') ?>"></script>
 	<script src="<?= base_url('metronic/assets/js/scripts.bundle.js') ?>"></script>
 	<!--end::Global Javascript Bundle-->
 	<!--begin::Page Custom Javascript(used by this page)-->
 
-	<script src="<?= base_url('metronic/assets/js/jquery-3.6.0.mn.js') ?>"></script>
 	<script src="<?= base_url('metronic/assets/plugins/custom/datatables/datatables.bundle.js') ?>"></script>
 
 
