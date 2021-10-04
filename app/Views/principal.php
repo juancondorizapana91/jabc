1d5a89ce6a79a0d12fa47f4ebfc484941e2790b5
EwinGoo <09alanocaramirez2000@gmail.com>
2021-10-03 22:22:05 -0400

    'cambios'

--- app/Views/principal.php
+++ app/Views/principal.php
@@ -151,15 +151,19 @@
                                         </span>
                                         <a href="#" class="text-warning fw-bold fs-6">Inscripciones semanales</a>
                                     </div>
-                                    <div class="col bg-light-primary px-6 py-8 rounded-2 mb-7">
-                                        <span class="svg-icon svg-icon-3x svg-icon-primary d-block my-2">
+                                    <div class="col bg-light-primary px-6 py-8 rounded-2 mb-7 ">
+                                        <div class="col">1</div>
+                                        <div class="col">2</div>
+                                        <span class="svg-icon svg-icon-3x svg-icon-primary my-2">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                                                 <path d="M18,8 L16,8 C15.4477153,8 15,7.55228475 15,7 C15,6.44771525 15.4477153,6 16,6 L18,6 L18,4 C18,3.44771525 18.4477153,3 19,3 C19.5522847,3 20,3.44771525 20,4 L20,6 L22,6 C22.5522847,6 23,6.44771525 23,7 C23,7.55228475 22.5522847,8 22,8 L20,8 L20,10 C20,10.5522847 19.5522847,11 19,11 C18.4477153,11 18,10.5522847 18,10 L18,8 Z M9,11 C6.790861,11 5,9.209139 5,7 C5,4.790861 6.790861,3 9,3 C11.209139,3 13,4.790861 13,7 C13,9.209139 11.209139,11 9,11 Z" fill="#000000" fill-rule="nonzero" opacity="0.3" />
                                                 <path d="M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero" />
                                             </svg>
                                         </span>
-                                        <a href="#" class="text-primary fw-bold fs-6">New Users</a>
+                                        <p class="text-end">1</p>
+                                        <a href="#" class="text-primary fw-bold fs-6 d-block">New Users</a>
                                     </div>
+
                                 </div>
                                 <div class="row g-0">
                                     <div class="col bg-light-danger px-6 py-8 rounded-2 me-7">
